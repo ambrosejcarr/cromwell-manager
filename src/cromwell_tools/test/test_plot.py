7d8ce37dbf1a760687d13780089ac77bbe6338ec
Ambrose J Carr <mail@ambrosejcarr.com>
2017-09-30 14:16:02 -0400

    added testing files

--- src/cromwell_tools/test/test_plot.py
+++ src/cromwell_tools/test/test_plot.py
@@ -3,15 +3,13 @@ import os
 from cromwell_tools import plot
 
 module_dir, module_name = os.path.split(__file__)
-
-test_log = module_dir + '/../../../utilizations/workflow4_utilization.txt'
+test_log = module_dir + '/data/utilizations/workflow4_utilization.txt'
 
 
 class TestPlot(unittest.TestCase):
 
     def test_parser(self):
-        with open(test_log, 'r') as f:
-            plot.log_parser(f)
+        raise NotImplementedError
 
 if __name__ == "__main__":
     unittest.main()
